fae77dcda78100080c97074216cd330bd6c665ef
ongka <karl_ong23@live.ca>
2022-05-10 21:00:11 -0400

    added fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from '../components/layouts/main.js';
+import Fonts from "../components/fonts.js";
 import theme from "../lib/theme.js";
 
 const Website = ( { Component, pageProps, router } ) => {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
